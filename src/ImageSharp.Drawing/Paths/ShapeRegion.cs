0e06ca2581c15b9412d28d13a5b5313536f98c8b
Fahad Alshaya <fahadabdulaziz@gmail.com>
2017-09-14 02:15:53 +0300

    Update

--- src/ImageSharp.Drawing/Paths/ShapeRegion.cs
+++ src/ImageSharp.Drawing/Paths/ShapeRegion.cs
@@ -49,7 +49,7 @@ namespace SixLabors.ImageSharp.Drawing
             using (var innerBuffer = new Buffer<PointF>(buffer.Length))
             {
                 PointF[] array = innerBuffer.Array;
-                int count = this.Shape.FindIntersections(start, end, array);
+                int count = this.Shape.FindIntersections(start, end, array, 0);
 
                 for (int i = 0; i < count; i++)
                 {
